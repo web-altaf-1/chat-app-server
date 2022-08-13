f7db389ae1d5244c302814a43e35d1b761fd8ae8
web-altaf-1 <web.altaf.1@gmail.com>
2022-08-14 00:55:25 +0600

    all added

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors'); 
-const {Server} = require('socket.io');
-
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:'http://localhost:3000',
-        methods:['GET','POST'],
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
     },
-})
+});
 
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-io.on("connection",(socket)=>{
-    console.log(`user connected , ${socket.id}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
 
-    socket.on('disconnect',()=>{
-        console.log(`user disconnected , ${socket.id}`);
-    })
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
 
-})
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
 
-
-server.listen(4000, ()=>{
-    console.log('server running');
-})
+server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+});
